bd88d397a7a6a3bd86e0900e7944db6faed9bf3c
DESKTOP-GTKSC19\gme <youngho7482@naver.com>
2023-04-10 18:03:07 +0900

    fourth of main page developing

---
+++ src/Main4.jsx
@@ -0,0 +1,59 @@
+const Main4 = () => {
+  return (
+    <>
+      <div
+        id="fws_6433b370e8990"
+        class="wpb_row vc_row-fluid vc_row full-width-section  "
+        style={{ paddingTop: 0, paddingBottom: 0 }}
+      >
+        <div
+          class="row-bg-wrap"
+          data-bg-animation="none"
+          data-bg-overlay="false"
+        >
+          <div class="inner-wrap using-image">
+            <div
+              class="row-bg using-image loaded"
+              style={{
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+          </div>
+        </div>
+        <div class="row_col_wrap_12 col span_12 dark left">
+          <div class="vc_col-sm-12 wpb_column column_container vc_column_container col no-extra-padding inherit_tablet inherit_phone ">
+            <div class="vc_column-inner">
+              <div class="wpb_wrapper">
+                <div class="divider-wrap height_tablet_60px ">
+                  <div style={{ height: 120 }} class="divider"></div>
+                </div>
+                <h1
+                  style={{ fontSize: 52, color: "#fffff", textAlign: "center" }}
+                  class="vc_custom_heading"
+                >
+                  Total finance service provider
+                </h1>
+                <div class="wpb_text_column wpb_content_element ">
+                  <div class="wpb_wrapper">
+                    <p style={{ textAlign: "center" }}>
+                      <span style={{ color: "#fff" }}>
+                        Easy and effortless money transfer that’s never been
+                        before.
+                      </span>
+                    </p>
+                  </div>
+                </div>
+
+                <div class="divider-wrap height_tablet_60px ">
+                  <div style={{ height: 120 }} class="divider"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Main4;
